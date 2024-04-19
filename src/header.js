d1f15dd87277242cc67efe9059a877e14f0ef5f1
DarkTrain06 <111948361+DarkTrain06@users.noreply.github.com>
2024-04-19 19:07:42 +0000

    Practica1

---
+++ src/header.js
@@ -0,0 +1,27 @@
+import React from "react";
+
+const Header = () => {
+  return (
+    <header style={{ backgroundColor: '#f2f2f2', borderTop: '5px solid #ff0000' }}>
+      <div className="container">
+        <div className="row">
+          <div className="col">
+            <h1>Compras finas</h1>
+          </div>
+          <div className="col">
+            <nav>
+              <ul className="d-flex justify-content-end">
+              <button type="button" class="btn btn-outline-primary">Productos</button>
+                <button type="button" class="btn btn-outline-success">Marcas</button>
+                <button type="button" class="btn btn-outline-warning">Accesorios</button>
+                <button type="button" class="btn btn-outline-info">Más</button>
+              </ul>
+            </nav>
+          </div>
+        </div>
+      </div>
+    </header>
+  );
+}
+
+export default Header;
---
+++ src/main.js
@@ -0,0 +1,33 @@
+import React, { useState } from "react";
+
+const Main = () => {
+  const [slideIndex, setSlideIndex] = useState(0);
+  const images = [
+    "https://imgs.search.brave.com/oOx3-2-0YJUy_jSi6p_1QZFhUZICdQXky1j4jRUUBF8/rs:fit:860:0:0/g:ce/aHR0cHM6Ly9odHRw/Mi5tbHN0YXRpYy5j/b20vRF9OUV9OUF83/OTQwNTItTUxNNzI3/Njg0NzYzMTlfMTEy/MDIzLVcud2VicA",
+    "https://imgs.search.brave.com/otRg_W5TGmsflIfrZwoSoiPg_NmsP4d6agPNk19E6QM/rs:fit:860:0:0/g:ce/aHR0cHM6Ly9jZG5h/Lmx5c3RpdC5jb20v/NDAwLzUwMC9jL3Bo/b3Rvcy9iYWxlbmNp/YWdhLzIzMzg5NWY0/L2JhbGVuY2lhZ2Et/YmxhY2stU3RlbmNp/bC10eXBlLWJhZ2d5/LWpvZ2dpbmdob3Nl/LmpwZWc",
+    "https://imgs.search.brave.com/Fntq-qHtlh9ajOZy55xkKCBj9xM5DHUWvl11KPKdwyc/rs:fit:860:0:0/g:ce/aHR0cHM6Ly9pbWFn/ZXMuc3R5bGlnaHQu/bmV0L2ltYWdlL3Vw/bG9hZC9lX3RyaW0v/dF93ZWJfcHJvZHVj/dF8zMzB4NDQwbWF4/X25vYmcvcV9hdXRv/OmVjbyxmX2F1dG8v/ZGVyMWM3dHJzMGpx/eW1jZHFuZnIud2Vi/cA"
+];
+
+const nextSlide = () => {
+  if (slideIndex < images.length - 1) {
+    setSlideIndex(slideIndex + 1);
+  } else {
+    setSlideIndex(0);
+  }
+};
+
+return (
+  <div className="container">
+    <div className="row">
+      <div className="col">
+        <div className="carousel-container">
+          <img src={images[slideIndex]} alt={`Imagen ${slideIndex + 1}`} />
+        </div>
+        <button onClick={nextSlide}>Siguiente</button>
+      </div>
+    </div>
+  </div>
+);
+};
+
+export default Main;
